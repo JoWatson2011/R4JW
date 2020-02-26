d3733604d9b6a14ba509eff241ad7b1ff0847f14
jowatson2011 <joanne.watson-5@postgrad.manchester.ac.uk>
2020-02-26 13:16:31 +0000

    simthresh parameter added

--- R/simplifyGO.R
+++ R/simplifyGO.R
@@ -5,6 +5,7 @@
 #'
 #' @param GORes dataframe with columns GOID and TERM
 #' @param simplifyData output of simplifyGOReqData
+#' @param simThresh threshold for semantic similarity
 #'
 #' @return filtered GORes dataframe
 #' @importFrom tidyr gather
@@ -14,34 +15,34 @@
 #'
 #' @examples exampleGO <- data.frame(TERM = c(), GOID=c())
 #' @examples simplifyGO(exampleGO)
-#' 
-simplifyGO <- function(GORes, simplifyData = simplifyGOReqData()){
-  
+#'
+simplifyGO <- function(GORes, simplifyData = simplifyGOReqData(), simThresh = 0.4){
+
   if(nrow(GORes) < 1){
     NA
   }else{
     GORes <- merge(GORes, simplifyData$GOterms, by = "TERM") %>% unique
-    
+
     sim <- mgoSim(GORes$GOID, GORes$GOID,
                   semData = simplifyData$semData,
                   measure="Rel",
                   combine=NULL)
-    
+
     sim[is.na(sim)] <- 0
-    
+
     ## to satisfy codetools for calling gather
     go1 <- go2 <- similarity <- NULL
-    
+
     sim.df <- as.data.frame(sim)
     sim.df$go1 <- row.names(sim.df)
     sim.df <- gather(sim.df, go2, similarity, -go1)
     sim.df <- sim.df[!is.na(sim.df$similarity),]
     sim.df <- sim.df[ order(sim.df$similarity,decreasing = T),]
-    
+
     #get the simiar term pairs
     sim.df <- sim.df[sim.df$go1 !=sim.df$go2,]
-    sim.df <- sim.df[sim.df$similarity > 0.4,]
-    
+    sim.df <- sim.df[sim.df$similarity > simThresh,]
+
     #mark high fequency terms
     sim.df$remove <- apply(sim.df,1,function(x) {
       if (x[1] %in% simplifyData$highFreqTerms){
@@ -55,16 +56,16 @@ simplifyGO <- function(GORes, simplifyData = simplifyGOReqData()){
     })
     remove<-na.omit(sim.df$remove)
     sim.df<-sim.df[is.na(sim.df$remove),]
-    
+
     if(nrow(sim.df) == 0){
-      NA
+      GORes.filt <- data.frame()
     }else{
       #iterate and remove the term with the least significant p-value
       sim.df$go1.pval<-GORes$adjustedP[match(sim.df$go1,GORes$GOID)]
       sim.df$go2.pval<-GORes$adjustedP[match(sim.df$go2,GORes$GOID)]
-      
+
       for (i in 1:nrow(sim.df)){
-        
+
         #check to see if the goterm has already been marked for removal
         if (sim.df[i,"go1"] %in% remove){
           next
@@ -72,10 +73,10 @@ simplifyGO <- function(GORes, simplifyData = simplifyGOReqData()){
         if (sim.df[i,"go2"] %in% remove){
           next
         }
-        
+
         go1.pval <- sim.df[i,"go1.pval"]
         go2.pval <- sim.df[i,"go2.pval"]
-        
+
         #if the p-values are equal then check if parent-child and keep the child term if so
         if (go1.pval==go2.pval){
           go1 <- sim.df[i,"go1"]
@@ -87,14 +88,13 @@ simplifyGO <- function(GORes, simplifyData = simplifyGOReqData()){
             remove<-c(remove,go1)
           next
         }
-        
+
         #remove least sig term
         remove<-c(remove,sim.df[i,which.max( c(go1.pval,go2.pval))])
       }
-      
+
       GORes.filt <- GORes[ !GORes$GOID %in% remove,]
-      
-      return(GORes.filt)
     }
+    return(GORes.filt)
   }
 }
